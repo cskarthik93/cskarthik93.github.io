f2a7860bfd197dbb4e6d4e053120d36bf11c6411
Karthik Selvaraj <cskarthik93@hotmail.com>
2024-03-19 16:46:48 -0400

    Using Email JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,34 +3,39 @@ const loadingDiv = document.querySelector('.loading');
 const errorMessage = document.querySelector('.error-message');
 const sentMessage = document.querySelector('.sent-message');
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
+form.addEventListener('submit', sendEmail);
+
+function sendEmail(event) {
+  event.preventDefault();
 
   // Show loading message
   loadingDiv.style.display = 'block';
   errorMessage.style.display = 'none';
   sentMessage.style.display = 'none';
 
-  const formData = new FormData(form);
+  // Get form values
+  const name = document.querySelector('input[name="name"]').value;
+  const email = document.querySelector('input[name="email"]').value;
+  const subject = document.querySelector('input[name="subject"]').value;
+  const message = document.querySelector('textarea[name="message"]').value;
 
-  fetch(/.netlify/functions/submit, {
-    method: 'POST',
-    body: formData
-  })
-  .then(response => response.text())
-  .then(data => {
+  // Send the email using EmailJS
+  emailjs.send('service_mw4jgdd', 'template_cl7aeoa', {
+    from_name: name,
+    from_email: email,
+    subject: subject,
+    message_html: message,
+  }, 'zC3-rTdaAcvL9Jt8F')
+  .then(() => {
     loadingDiv.style.display = 'none';
-    console.log(data);
-    if (data === 'success') {
-      sentMessage.style.display = 'block';
-    } else {
-      errorMessage.innerHTML = data;
-      errorMessage.style.display = 'block';
-    }
+    sentMessage.style.display = 'block';
+    // Reset form fields
+    document.querySelector('form').reset();
   })
-  .catch(error => {
+  .catch((error) => {
     loadingDiv.style.display = 'none';
-    errorMessage.innerHTML = 'An error occurred. Please try again later.';
+    errorMessage.innerHTML = 'An error occurred while sending the email. Please try again later.';
     errorMessage.style.display = 'block';
+    console.error('Error sending email:', error);
   });
-});
+}
